e68f948ef2ce65deba1c04b73deaaca26adc945d
Murray Christopherson <murray.christopherson@hipplay.com>
2014-12-03 10:42:57 -0500

    Implemented the breadth first implementation of the code, so it is more similar to how it was done in the video

--- rationalnumbers.go
+++ rationalnumbers.go
@@ -3,6 +3,7 @@ package main
 // Inspired by http://youtu.be/DpwUVExX27E
 
 import (
+	"container/list"
 	"fmt"
 )
 
@@ -10,7 +11,7 @@ const (
 	MaxDepth = 6 // The code is exponential, so if this is too high, expect to wait for years before your answer comes back
 )
 
-func step(numerator int, denominator int, currentDepth uint, maxDepth uint) {
+func generate_depthFirst(numerator uint, denominator uint, currentDepth uint, maxDepth uint) {
 	if currentDepth >= maxDepth {
 		return
 	}
@@ -18,10 +19,42 @@ func step(numerator int, denominator int, currentDepth uint, maxDepth uint) {
 	fmt.Printf("%d/%d = %f\n", numerator, denominator, value)
 	sum := numerator + denominator
 	newDepth := currentDepth + 1
-	step(numerator, sum, newDepth, maxDepth)
-	step(sum, denominator, newDepth, maxDepth)
+	generate_depthFirst(numerator, sum, newDepth, maxDepth)
+	generate_depthFirst(sum, denominator, newDepth, maxDepth)
+}
+
+type Vertex struct {
+	numerator   uint
+	denominator uint
+}
+
+const (
+	MaxVertexes = 40
+)
+
+func generate_breadthFirst(maxVertexes uint) {
+	queue := list.New()
+	queue.PushBack(&Vertex{1, 1})
+	var step uint = 1
+	for e := queue.Front(); e != nil; e = e.Next() {
+		v := e.Value.(*Vertex)
+		sum := v.numerator + v.denominator
+		if step < maxVertexes {
+			queue.PushBack(&Vertex{v.numerator, sum})
+			step++
+		}
+		if step < maxVertexes {
+			queue.PushBack(&Vertex{sum, v.denominator})
+			step++
+		}
+		fmt.Printf("%d / %d = %g\n", v.numerator, v.denominator, float64(v.numerator)/float64(v.denominator))
+	}
 }
 
 func main() {
-	step(1, 1, 0, MaxDepth)
+	fmt.Println("Depth First Generation")
+	generate_depthFirst(1, 1, 0, MaxDepth)
+	fmt.Println()
+	fmt.Println("Breadth First Generation")
+	generate_breadthFirst(MaxVertexes)
 }
